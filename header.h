e236b409911ff7d05d5db1717ab1e4dfed51324a
ncomerci <ncomerci@itba.edu.ar>
2018-11-27 22:59:20 -0300

    archivos iniciales

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef _HEADER_H_
+
+	#define _HEADER_H_
+
+	#include <stdio.h>
+
+	#include <stdlib.h>
+
+	#include <ctype.h>
+
+	#include <math.h>
+
+	#include <string.h>
+
+	#include <time.h>
+
+	#include "getnum.h"
+
+	#include "rand.h"
+
+#endif
