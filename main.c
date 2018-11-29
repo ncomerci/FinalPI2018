d2c53fa0b63d8b16aad6d7dfd013b20a4041f15c
ncomerci <ncomerci@itba.edu.ar>
2018-11-28 22:22:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,21 @@
 
 int main(int cantArgs, char *args[]){
 
-
 	if(cantArgs != 3)
 	{
-		fprintf(stderr, "%s\n", "Two arguments expected.");
+		fprintf(stderr, "%s\n", "ERROR: Two arguments expected.");
 		exit(EXIT_FAILURE);
 	}	
 
-	int errnum;
+	FILE *file1 = fopen(args[1], "r+t");
+	FILE *file2 = fopen(args[2], "r+t");
+
+	if(errno != 0)
+	{
+		fprintf(stderr, " ERROR %s: %s\n", file1 == NULL ? args[1] : args[2], strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
 
 	return 0;
 }
