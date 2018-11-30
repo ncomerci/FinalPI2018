08fd450890f71e1219ad836f0ac617f621556d49
luciatorrusio <lucia.torusio@gmail.com>
2018-11-30 15:33:54 -0300

    Update mainlu.c

--- mainlu.c
+++ mainlu.c
@@ -32,7 +32,32 @@ int main(int cantArgs, char *args[]){
 	{
 		moves = file2;
 		airports = file1;
+	
+
+	char *datos[6];
+	char line[MAXCHAR +1];
+	while(!feof(moves)){
+		fegets(line, MAXCHAR+1, moves);
+		lineToStrings(datos,line);
+		agregamov(datos[2], datos[1], l);
+		MoveByDay(l,datos[0], datos[2]);
 	}
+
+
 
 	return 0;
 }
+
+void lineToStrings(char **datos, char* string){
+	int k=0;
+	char * Spointer;
+	Spointer=strtok(string, ";");
+	for (int i = 0; i < 7; ++i)
+	{
+		if(i!=1 && i!=4)
+			datos[k++]=Spointer;
+		Spointer=strtok(NULL, ";");
+	}
+
+}
+
