32f288ceb09330363d2c40d477989a7d3a54ec32
luciatorrusio <lucia.torusio@gmail.com>
2018-11-30 15:00:39 -0300

    Create mainlu.c

---
+++ mainlu.c
@@ -0,0 +1,38 @@
+#include "header.h"
+
+int main(int cantArgs, char *args[]){
+
+	if(cantArgs != 3)
+	{
+		fprintf(stderr, "ERROR: %s\n", "Two arguments expected.");
+		exit(EXIT_FAILURE);
+	}	
+
+	FILE *file1 = fopen(args[1], "r+t");
+	FILE *file2 = fopen(args[2], "r+t");
+
+	if(errno != 0)
+	{
+		fprintf(stderr, "ERROR: %s %s\n", file1 == NULL ? args[1] : args[2], strerror(errno));
+		exit(errno);
+	}
+
+	FILE *moves;
+	FILE *airports;
+	char firstWord[6];
+
+	fscanf(file1, "%[^;]", firstWord);
+
+	if(strcmp(firstWord, "Fecha") == 0)
+	{
+		moves = file1;
+		airports = file2;
+	}
+	else
+	{
+		moves = file2;
+		airports = file1;
+	}
+
+	return 0;
+}
