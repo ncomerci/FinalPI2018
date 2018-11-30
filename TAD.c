1fec5fa326eeec59d6717400df0a0d6e25579305
ncomerci <ncomerci@itba.edu.ar>
2018-11-29 23:43:18 -0300

    algunos arreglos en las funciones

--- TAD.c
+++ TAD.c
@@ -1,6 +1,7 @@
 #include "header.h"
 
 enum DAYS {SUN = 0, MON, TUE, WED, THU, FRI, SAT};
+enum FLIGHTTYPE {CAB = 0, INTER};
 
 typedef struct Tmove
 {
@@ -68,71 +69,89 @@ void MoveByDay(dataADT l, const char *date, const char *flightType){
 /*q3*/
 
 void agregamov(const char * ClasificVuelo, const char * clasVuelo, DataADT data){
-	if(strcmp(ClasificVuelo, "Cabotaje") == 0){
+
+	if(strcmp(ClasificVuelo, "Cabotaje") == 0)
+	{
 		if (strcmp(clasVuelo, "Regular") == 0)
-			data->movComp->Reg++; //movComp es un vector, faltaria el indice? (nico)
+			data->movComp[CAB].Reg++;
+
 		else if (strcmp(clasVuelo, "No Regular") == 0)
-			data->movComp->noReg++; //movComp es un vector, faltaria el indice? (nico)
+			data->movComp[CAB].noReg++;
+
 		else
-			data->movComp->priv++;
+			data->movComp[CAB].priv++;
 	}
-	else{
+	else
+	{
 		if (strcmp(clasVuelo == "Regular") == 0)
-			data->movComp[1].Reg++;
+			data->movComp[INTER].Reg++;
+
 		else if (strcmp(clasVuelo, "No Regular") == 0)
-			data->movComp[1].noReg++;
+			data->movComp[INTER].noReg++;
+		
 		else
-			data->movComp[1].priv++;
+			data->movComp[INTER].priv++;
 	}
 }
 
 /*Q1*/
-Pnode AddR(Pnode n,char * s1,char * s2){
-	Pnode aux=calloc(1,sizeof(Tnode));
-	strcpy(aux->oaci,s1);	
-	aux->denom=malloc(strlen(s2)+1);
-	strcpy(aux->denom,s2);
-	if(n!=NULL){
-	aux->prev=n;
-	n->next=aux;
-	}
-return aux;
-}
+dataADT addAirport(dataADT head, char * s1, char * s2){
 
-dataADT add(dataADT head, char * s1,char * s2){
-	head->last=AddR(head->last,s1,s2);
-	if(head->first==NULL)
+	Pnode aux = calloc(1, sizeof(Tnode));
+	strcpy(aux->oaci, s1);	
+	aux->denom = malloc(strlen(s2) + 1);
+	strcpy(aux->denom, s2);
+
+	if(head->last != NULL)
+	{
+		aux->prev = head->last;
+		head->last->next = aux;
+	}
+
+	head->last = aux;
+	
+	if(head->first == NULL)
 		head->first=head->last;
+
 	return head;
 }
 
-Pnode addCantR(Pnode n,char * s1,dataADT head){
-	if(n==NULL){
+static Pnode addCantR(Pnode n, const char * s1, dataADT head)
+{
+	if(n == NULL){
 		return n;
 	}
-	if(strcmp(s1,n->oaci)!=0){
-		n->next=addCantR(n->next,s1,head);
-		int c;
-		Pnode aux=n->next;
-		if((aux!=NULL && (c=aux->total-n->total)>0)||(c==0 && (c=strcmp(s1,n->oaci))<0)){
-			if(aux->next==NULL)
-				head->last=n;
-			aux->prev=n->prev;
-			n->next=aux->next;
-			n->prev=aux;
-			aux->next=n;
+
+	if(strcmp(s1, n->oaci) != 0)
+	{	
+		n->next = addCantR(n->next, s1, head);
+		int c;		
+		Pnode aux = n->next;
+		
+		if((aux != NULL && (c = aux->total - n->total) > 0) || ( c == 0 && strcmp(s1, n->oaci) < 0))
+		{
+			if(aux->next == NULL)
+				head->last = n;
+			
+			aux->prev = n->prev;
+			n->next = aux->next;
+			n->prev = aux;
+			aux->next = n;
+			
 			return aux;
 		}
+		
 		return n;
 	}
+	
 	n->total++;
+	
 	return n;
-
 }
 
-dataADT addCant(dataADT head,char * s1){
-	head->first=addCantR(head->first,s1,head);
-	return head;
+void addCant(dataADT head, char * s1)
+{
+	head->first = addCantR(head->first,s1,head);
 }
 
 void
@@ -143,7 +162,6 @@ skipLine(FILE *fp){
    do
    {
       c = fgetc(fp);
-      pos++;
    }
    while(c != '\n' && c != EOF);
 
