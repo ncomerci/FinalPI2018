16d693775a3a294334a4abceed23186958028e8d
luciatorrusio <lucia.torusio@gmail.com>
2018-11-30 18:24:58 -0300

    Create free.c

---
+++ prueba lectura/free.c
@@ -0,0 +1,31 @@
+
+void
+freeList( listADT list)
+{
+	Pnode curr=list->first;
+	Pnode aux;
+	Pmove aux2;
+	Pcomp aux3;
+
+	while (curr != NULL) {
+		aux = curr->tail;
+		free(curr);
+		curr = aux;
+	}
+	for (int i = 0; i < 7; ++i)
+	{
+		aux2=list->movDays[i];
+		free(aux);
+	}
+	for (int i = 0; i < 2; ++i)
+	{
+		aux3=list->movComp[i];
+		free(aux);
+	}
+
+
+	free(list->movComp);
+	free(list->movDays);
+	free(list);
+}
+
